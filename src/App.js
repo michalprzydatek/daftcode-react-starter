31536e3feb749f5efa8e93a255f7ec7cce46539d
michalprzydatek <michalprzydatek@gmail.com>
2018-05-07 16:41:07 +0200

    poprawa odliczania

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import { hot } from 'react-hot-loader';
 import * as React from 'react';
-import Home from './view/Home';
-import Count from './view/Count';
-
 import './styles/theme.sass';
+
+import Header from './view/Header';
+import LauncherDetails from './view/LauncherDetails';
+import Footer from './view/Footer';
+
+import launch from './assets/launch.json';
+import launchSite from './assets/launch_site.json';
+import rocket from './assets/rocket.json';
+
+import { format, formatDistance, formatRelative, subDays } from 'date-fns';
 
 class App extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
       <main>
-        <Home username="DaftCoder" />
-        <Count from={60} to={0} />
+        <Header username="DaftCoder" />
+        <LauncherDetails
+          launch={launch}
+          rocket={rocket}
+          launchSite={launchSite}
+        />
+        <Footer username="DaftCoder" />
 
       </main>
     );
