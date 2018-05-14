5dfd19bb1edab5a20aa6b21a3cde7d44053fa06b
michalprzydatek <michalprzydatek@gmail.com>
2018-05-14 13:13:37 +0200

    update

--- src/App.js
+++ src/App.js
@@ -2,28 +2,66 @@ import { hot } from 'react-hot-loader';
 import * as React from 'react';
 import './styles/theme.sass';
 
-import Header from './view/Header';
-import LauncherDetails from './view/LauncherDetails';
-import Footer from './view/Footer';
+import { format, formatDistance, formatRelative, subDays } from 'date-fns';
 
 import launch from './assets/launch.json';
 import launchSite from './assets/launch_site.json';
 import rocket from './assets/rocket.json';
 
-import { format, formatDistance, formatRelative, subDays } from 'date-fns';
+import LaunchDetails from 'view/LaunchDetails';
+
+import launches from './assets/launches.json';
+import LaunchesList from 'view/LaunchesList';
 
 class App extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewName: 'list',
+    };
+
+    this.handleLaunchClick = this.handleLaunchClick.bind(this);
+    this.handleBackClick = this.handleBackClick.bind(this);
+  }
+
+  get activeViewComponent() {
+    const { viewName } = this.state;
+
+    switch (viewName) {
+      case 'list':
+        return (
+          <LaunchesList
+            launches={launches}
+            onLaunchClick={this.handleLaunchClick}
+          />
+        );
+
+      case 'details':
+        return (
+          <LaunchDetails
+            launch={launch}
+            launchSite={launchSite}
+            rocket={rocket}
+            onBackClick={this.handleBackClick}
+          />
+        );
+
+      default: return null;
+    }
+  }
+
+  handleLaunchClick() {
+    this.setState({ viewName: 'details' });
+  }
+
+  handleBackClick() {
+    this.setState({ viewName: 'list' });
+  }
+
   render() {
     return (
       <main>
-        <Header username="DaftCoder" />
-        <LauncherDetails
-          launch={launch}
-          rocket={rocket}
-          launchSite={launchSite}
-        />
-        <Footer username="DaftCoder" />
-
+        {this.activeViewComponent}
       </main>
     );
   }
