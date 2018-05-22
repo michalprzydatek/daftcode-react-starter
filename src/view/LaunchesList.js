c14e4cb09a40fdf1b9e56b125e60f88e45bec21a
michalprzydatek <michalprzydatek@gmail.com>
2018-05-22 12:45:45 +0200

    update 5

--- src/view/LaunchesList.js
+++ src/view/LaunchesList.js
@@ -20,55 +20,121 @@ class LaunchesList extends React.Component {
     this.state = { availableRocketList: '', rocketNameFilter: 'all', isLoading:false};
   }
 
-  get allRocketNames() {  
+  get allRocketNames() {
     return <li className={this.state.rocketNameFilter === 'all' ? "active" : ""} onClick={(x)=>{this.handleFilterChange('all')}}><a href={"#"}>All rockets</a></li>;
   }
 
   get availableRocketNames() {
     let array = [];
+    let uniqueArray = [];
+    let uniqueArrayLi = [];
 
     fetch('https://api.spacexdata.com/v2/launches')
      .then((response) => response.json())
-     .then((data) => {
-       return data.map((e)=> array.push(e.rocket.rocket_name));
-     })
+     .then((data) => {       
+        data.map((e)=> array.push(e.rocket.rocket_name));
+        console.log('array',array);
+
+        }).then((data)=>{
+
+          uniqueArray = array.filter(function(item, pos) {
+            console.log('ua',item, pos )
+            array.indexOf(item) == pos;
+          });
+          console.log('uniqueArray',uniqueArray);
+
+        }).then((data)=>{
+          return uniqueArray.map((e,i) => 
+          <li className={this.state.rocketNameFilter === e ? 'active' : ''}>
+            <a href="#" data-name={e} onClick={(x)=>{this.handleFilterChange(e)}}>{e}</a>
+          </li>);
+        })
      .catch((error) => {
        console.error(error);
      });
-    
-    
-    
-    console.log('array',array);
-    
+
     //const rocketNames = names.map((e) => array.push(e.rocket.rocket_name));
-    const uniqueArray = array.filter(function(item, pos) {
-          return array.indexOf(item) == pos;
-    });
-    const uniqueArrayLi = uniqueArray.map((e,i) => 
-        <li className={this.state.rocketNameFilter === e ? 'active' : ''}>
-        <a href="#" data-name={e} onClick={(x)=>{this.handleFilterChange(e)}}>{e}</a>
-  </li>);
-    return uniqueArrayLi;
   }
   
   get availableRocketList() {
-    const {rocketNameFilter} = this.state;
-    let fetch_data = '';
-    let data_f='';
-    let dataFiltred_f ='';
 
-    let filtred = this.state.rocketNameFilter;
+    const {rocketNameFilter } = this.state;
+    console.log('availableRocketList',this.state);
 
     fetch('https://api.spacexdata.com/v2/launches')
-     .then((response) => response.json())
-     .then((data) => {
-        const launches = data;
-        console.log(launches);
-        this.setState({ launches:launches });
-        this.setState({ isLoading:false });
-        console.log('this.state.launches',this.state.launches);
-        
-        data_f = this.state.launches.map((e) =>
+    .then((response) => response.json())
+    .then((data) => {
+        console.log('availableRocketList',data.length);
+
+      if(rocketNameFilter ==='all'){
+        console.log('all');
+        return (
+          data.map((e) =>
+          <li>
+            <div className={"rockets__content"}>
+              <div className={"rockets__content__date"}>{format( new Date(e.launch_date_local), 'DD MMMM YYYY' )}</div>
+              <div className={"rockets__content__details"}>
+                <div className={"rockets__details__lab-name"}>Rocket: </div>
+                <div className={"rockets__details__name"}><a href="#" onClick={this.props.onLaunchClick}>{e.rocket.rocket_name}</a></div>
+                <div className={"rockets__details__lab-site"}>|&nbsp;&nbsp; Launch Site: </div>
+                <div className={"rockets__details__site"}>{e.launch_site.site_name_long}</div>
+              </div>
+            </div>
+          </li>
+          )
+        )
+      }else{
+        console.log('no all');
+        return (
+          data.filter( launch => launch.rocket.rocket_name === rocketNameFilter ).map((e) =>
+            <li>
+              <div className={"rockets__content"}>
+                <div className={"rockets__content__date"}>{format( new Date(e.launch_date_local), 'DD MMMM YYYY' )}</div>
+                <div className={"rockets__content__details"}>
+                  <div className={"rockets__details__lab-name"}>Rocket: </div>
+                  <div className={"rockets__details__name"}><a href="#" onClick={this.props.onLaunchClick}>{e.rocket.rocket_name}</a></div>
+                  <div className={"rockets__details__lab-site"}>|&nbsp;&nbsp; Launch Site: </div>
+                  <div className={"rockets__details__site"}>{e.launch_site.site_name_long}</div>
+                </div>
+              </div>
+            </li>
+          )
+        )  
+      }
+      
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
+    
+/*
+    fetch('https://api.spacexdata.com/v2/launches')
+      .then((response) => response.json())
+      .then((data) => {
+        const launches_d = data;
+        console.log(launches_d);
+        this.setState({ launches:launches_d });
+        this.setState({ isLoading: false });
+        console.log('availableRocketList, this.state.launches',this.state.launches);
+
+        let filtred = this.state.rocketNameFilter;
+        //const data_f = 'aaa';
+        const data_f = this.state.launches.map((e) =>
+                    <li>
+                      <div className={"rockets__content"}>
+                        <div className={"rockets__content__date"}>{format(  new Date(e.launch_date_local), 'DD MMMM YYYY' )}</div>
+                        <div className={"rockets__content__details"}>
+                          <div className={"rockets__details__lab-name"}>Rocket: </div>
+                          <div className={"rockets__details__name"}><a href="#" onClick={this.props.onLaunchClick}>{e.rocket.rocket_name}</a></div>
+                          <div className={"rockets__details__lab-site"}>|&nbsp;&nbsp; Launch Site: </div>
+                          <div className={"rockets__details__site"}>{e.launch_site.site_name_long}</div>
+                        </div>
+                      </div>
+                    </li>
+            )
+
+            const dataFiltred_f = this.state.launches.filter( launch => launch.rocket.rocket_name === rocketNameFilter ).map((e) =>
             <li>
               <div className={"rockets__content"}>
                 <div className={"rockets__content__date"}>{format(  new Date(e.launch_date_local), 'DD MMMM YYYY' )}</div>
@@ -79,48 +145,58 @@ class LaunchesList extends React.Component {
                   <div className={"rockets__details__site"}>{e.launch_site.site_name_long}</div>
                 </div>
               </div>
-            </li>
-    )
-    
-      dataFiltred_f = this.state.launches.filter( launch => launch.rocket.rocket_name === rocketNameFilter ).map((e) =>
-              <li>
-                <div className={"rockets__content"}>
-                  <div className={"rockets__content__date"}>{format(  new Date(e.launch_date_local), 'DD MMMM YYYY' )}</div>
-                  <div className={"rockets__content__details"}>
-                    <div className={"rockets__details__lab-name"}>Rocket: </div>
-                    <div className={"rockets__details__name"}><a href="#" onClick={this.props.onLaunchClick}>{e.rocket.rocket_name}</a></div>
-                    <div className={"rockets__details__lab-site"}>|&nbsp;&nbsp; Launch Site: </div>
-                    <div className={"rockets__details__site"}>{e.launch_site.site_name_long}</div>
-                  </div>
-                </div>
-              </li>   
-      )
+            </li> 
+            )
+            
+        
       })
-     .catch((error) => {
-       console.error(error);
-     });
-
-     
-    return rocketNameFilter === 'all' ? data_f : dataFiltred_f ;
+      .catch((error) => {
+        console.error(error);
+      });
+  */
+   
+    
   }
 
   get filteredLaunches(){
     const {rocketNameFilter} = this.state;
+    console.log('filteredLaunches',this.state);
+
+    if(!rocketNameFilter) return launches;
+
+    fetch('https://api.spacexdata.com/v2/launches?rocket_id'+rocketNameFilter)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log('filteredLaunches',data.filter( launch => launch.rocket.rocket_name === rocketNameFilter ));
+      return (
+        data.filter( launch => launch.rocket.rocket_name === rocketNameFilter )
+      )
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
+    /*
     const launches = this.getData('https://api.spacexdata.com/v2/launches');
 
     if(!rocketNameFilter) return launches;
 
     //return launches.filter( launch => launch.rocket.rocket_name === rocketNameFilter );
-    return this.getData('https://api.spacexdata.com/v2/launches?rocket_id='+rocketNameFilter);
+    
+    */
   }
 
   doToDetails(){
+    console.log('doToDetails',this.state);
+    /*
     const {rocketNameFilter} = this.state;
     const launches = this.getData('https://api.spacexdata.com/v2/launches');
 
     if(!rocketNameFilter) return launches;
 
     return launches.filter( launch => launch.rocket.rocket_name === rocketNameFilter );
+    */
+    return ['a','b','c']
   }
 
   handleFilterChange(value) {
@@ -129,19 +205,26 @@ class LaunchesList extends React.Component {
     return false;
   }
 
-getData(url) {
-   return fetch(url)
-   .then((response) => response.json())
-   .then((responseJson) => {
-     return responseJson;
-   })
-   .catch((error) => {
-     console.error(error);
-   });
-}
   componentDidMount() {
-    this.setState({isLoading: true});
+
+    fetch('https://api.spacexdata.com/v2/launches')
+      .then((response) => response.json())
+      .then((data) => {
+        const launches_d = data;
+        console.log(launches_d);
+        this.setState({ launches:launches_d });
+        this.setState({ isLoading: false });
+        console.log('componentDidMount, this.state.launches',this.state.launches);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
     this.getInitialState();
+  }
+
+  componentDidUpdate() {
+    console.log(  'componentDidUpdate'  );
   }
 
   componentWillUnmount() {
